38d1ed1a956ce53531f759b54c364a3382d1e8ef
Alex <alex@alex-dev.me>
2023-12-13 11:59:01 -0600

    Refactor HeroScreen component to include navigation and display hero details

--- src/heroes/pages/HeroScreen.jsx
+++ src/heroes/pages/HeroScreen.jsx
@@ -1,6 +1,44 @@
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 export const HeroScreen = () => {
-  const params = useParams();
-  return <h1>HeroScreen</h1>;
+  const { id } = useParams();
+  const hero = useMemo(() => getHeroById(id), [id]);
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div className="container mt-5">
+      <div className="flex justify-center items-center gap-4 rounded-md shadow-md p-4">
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="w-1/2 rounded-md shadow-md aspect-w-16 aspect-h-9 object-cover max-w-sm"
+        />
+        <div>
+          <h1 className="text-2xl font-bold">{`HeroScreen ${hero.superhero}`}</h1>
+          <ul>
+            <li className="list-none">{`Alter ego: ${hero.alter_ego}`}</li>
+            <li className="list-none">{`Publisher: ${hero.publisher}`}</li>
+            <li className="list-none">{`Primera aparición: ${hero.first_appearance}`}</li>
+          </ul>
+          <h5 className="text-xl font-bold">Characters</h5>
+          <p className="text-base">{hero.characters}</p>
+          <button
+            className="btn btn-primary mt-4 text-white"
+            onClick={onNavigateBack}
+          >
+            Return
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
