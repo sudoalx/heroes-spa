a82e61f5dcb1667e7b858b9817c785f3bdb70f3b
Alex <alex@alex-dev.me>
2023-12-13 11:58:52 -0600

    Refactor HeroList component to use useMemo hook

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,8 +1,9 @@
+import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers";
 import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {heroes.map((hero) => (
