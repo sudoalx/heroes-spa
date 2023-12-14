dc15d8b985e5a191cc72a41a670eaa9f76f1c02f
Alex <alex@alex-dev.me>
2023-12-13 22:56:37 -0600

    Update page title in DC.jsx

--- src/heroes/pages/Dc.jsx
+++ src/heroes/pages/Dc.jsx
@@ -3,7 +3,7 @@ import { HeroList } from "../components/";
 export const Dc = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">DCPage</h1>
+      <h1 className="text-2xl font-bold mb-4">DC Heroes</h1>
       <HeroList publisher={"DC Comics"} />
     </div>
   );
