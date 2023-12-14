8cb63e325e2095ce996b19516a9768da9ab85254
Alex <alex@alex-dev.me>
2023-12-13 22:57:28 -0600

    Update Navbar component to include additional links and improve responsiveness

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -12,6 +12,15 @@ export const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
+        {/* large screens */}
+        <div className="hidden md:block">
+          <NavLink to={"/"} className="btn btn-ghost text-xl">
+            Home
+          </NavLink>
+        </div>
+        {/* end large screens */}
+
+        {/* small screens */}
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
             <svg
@@ -31,7 +40,7 @@ export const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[100] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
               <NavLink to={"marvel"}>Marvel</NavLink>
@@ -39,15 +48,54 @@ export const Navbar = () => {
             <li>
               <NavLink to={"dc"}>DC</NavLink>
             </li>
+            <li>
+              <NavLink to={"search"}>Search</NavLink>
+            </li>
           </ul>
         </div>
+        {/* end small screens */}
       </div>
+
       <div className="navbar-center">
+        {/* large screens */}
+        <div className="form-control lg:mx-4 lg:w-64 hidden md:block">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        {/* end large screens */}
+        {/* small screens */}
         <NavLink to={"/"} className="btn btn-ghost text-xl">
           Heroes App
         </NavLink>
+        {/* end small screens */}
       </div>
       <div className="navbar-end">
+        {/* large screens */}
+        <ul
+          tabIndex={0}
+          className="navbar-nav md:flex md:flex-row lg:gap-4 hidden"
+        >
+          <li className="">
+            <NavLink to={"marvel"} className="btn btn-ghost">
+              Marvel
+            </NavLink>
+          </li>
+          <li className="">
+            <NavLink to={"dc"} className="btn btn-ghost">
+              DC
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={"search"} className="btn btn-ghost">
+              Search
+            </NavLink>
+          </li>
+        </ul>
+
+        {/* end large screens */}
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
@@ -55,10 +103,7 @@ export const Navbar = () => {
             className="btn btn-ghost btn-circle avatar"
           >
             <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-              />
+              <img alt="Avatar" src="/assets/avatar.svg" />
             </div>
           </div>
           <ul
@@ -89,5 +134,3 @@ export const Navbar = () => {
     </div>
   );
 };
-
-// className={({ isActive }) => ` ${isActive ? "active" : ""}`}
