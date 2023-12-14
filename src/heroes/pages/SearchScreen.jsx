af40b3d97c0f95cb267338908fa81a9e6f55b8f4
Alex <alex@alex-dev.me>
2023-12-13 22:57:10 -0600

    Add search functionality to SearchScreen component

--- src/heroes/pages/SearchScreen.jsx
+++ src/heroes/pages/SearchScreen.jsx
@@ -1,3 +1,65 @@
+import React from "react";
+import { HeroCard } from "../components/";
+import { useForm } from "../../hooks/useForm";
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { getHeroesByName } from "../helpers";
+
 export const SearchScreen = () => {
-  return <h1>SearchScreen</h1>;
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const query = queryString.parse(location.search);
+
+  const { q = "" } = query;
+
+  const heroes = getHeroesByName(q);
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q,
+  });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    if (searchText.trim().length > 50) return;
+    navigate(`?q=${searchText}`);
+  };
+
+  return (
+    <div className="p-4">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-4">Search</h1>
+        <form onSubmit={onSearchSubmit}>
+          <input
+            type="text"
+            name="searchText"
+            className="input border border-gray-400 rounded p-2 focus:outline-none focus:border-primary"
+            placeholder="Searching..."
+            autoComplete="off"
+            value={searchText}
+            onChange={onInputChange}
+          />
+        </form>
+      </div>
+      <h2 className="text-2xl font-bold mt-4 mb-4">Results</h2>
+      {(heroes.length === 0 &&
+        (q.length === 0 ? (
+          <p className="align-middle text-center">
+            Start searching by typing the name of the hero in the search box.
+          </p>
+        ) : (
+          <p className="align-middle text-center wrap">
+            There are no heroes with the name "<strong>{q}</strong>" in the DB.
+          </p>
+        ))) ||
+        (heroes.length > 0 && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {heroes.map((hero) => (
+              <HeroCard key={hero.id} {...hero} />
+            ))}
+          </div>
+        ))}
+    </div>
+  );
 };
