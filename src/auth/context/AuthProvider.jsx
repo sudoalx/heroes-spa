9db3e0f30e9f07d229c27f917ea07c06d4267853
Alex <alex@alex-dev.me>
2023-12-15 02:20:56 -0600

    Refactor AuthProvider component to include login and logout functionality

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,11 +1,49 @@
 import { useReducer } from "react";
 import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
+import { types } from "../types/types";
 
 export const AuthProvider = ({ children }) => {
-  const initialState = {
-    logged: false,
+  const init = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    return {
+      logged: !!user,
+      user: user,
+    };
   };
-  const [authState, authDispatch] = useReducer(authReducer, initialState);
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+
+  const [authState, authDispatch] = useReducer(authReducer, {}, init);
+
+  const login = (name = "") => {
+    const user = { id: "123ABC", name: "Alex" };
+
+    const action = {
+      type: types.login,
+      payload: user,
+    };
+
+    localStorage.setItem("user", JSON.stringify(user));
+
+    authDispatch(action);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    const action = {
+      type: types.logout,
+    };
+    authDispatch(action);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...authState,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
