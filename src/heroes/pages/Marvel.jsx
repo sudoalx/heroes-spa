a2725a7ea6eec43b123ebfdfdd6beb7d610548de
Alex <alex@alex-dev.me>
2023-12-15 11:16:28 -0600

    Update Marvel page heading

--- src/heroes/pages/Marvel.jsx
+++ src/heroes/pages/Marvel.jsx
@@ -3,7 +3,7 @@ import { HeroList } from "../components/";
 export const Marvel = () => {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Marvel Heroes</h1>
+      <h1 className="text-4xl font-bold mb-4 text-center">Marvel Comics</h1>
       <HeroList publisher={"Marvel Comics"} />
     </div>
   );
