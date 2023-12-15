ed56e49a6431e7cd35ddac29afdeaf0d8cadc053
Alex <alex@alex-dev.me>
2023-12-15 02:57:31 -0600

    Refactor routes in AppRouter.jsx Added public and private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../auth/pages/Login";
-import { HeroesRoute } from "../heroes/routes/HeroesRoute";
+import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<HeroesRoute />} />
+      <Route
+        path="/login"
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+      ></Route>
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        }
+      ></Route>
     </Routes>
   );
 };
