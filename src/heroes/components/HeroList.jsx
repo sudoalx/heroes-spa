ade8fffdfd0e92b5f1f2d142e75fc453790464e4
Alex <alex@alex-dev.me>
2023-12-15 11:16:12 -0600

    Update HeroList grid layout

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -5,7 +5,7 @@ import { HeroCard } from "./HeroCard";
 export const HeroList = ({ publisher }) => {
   const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-9/12 mx-auto">
       {heroes.map((hero) => (
         <HeroCard key={hero.id} {...hero} />
       ))}
