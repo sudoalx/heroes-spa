42a2169353de2d6a1ae1c08c413b8a4d5c448ebc
Alex <alex@alex-dev.me>
2023-12-19 11:44:47 -0600

    Add tests for PublicRoute component

---
+++ tests/router/PublicRoute.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../../src/auth/context/AuthContext";
+import { PublicRoute } from "../../src/router/PublicRoute";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
+
+describe("Tests on public route", () => {
+  test("should return children if not authenticated", () => {
+    const contextValue = {
+      logged: false,
+    };
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <PublicRoute>
+          <h1>Public Route</h1>
+        </PublicRoute>
+      </AuthContext.Provider>
+    );
+
+    expect(screen.getByText("Public Route")).toBeTruthy();
+  });
+
+  test("should navigate if it is authenticated", () => {
+    const contextValue = {
+      logged: true,
+      user: {
+        name: "Alex",
+        id: "1234",
+      },
+    };
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/login"]}>
+          <Routes>
+            <Route
+              path="login"
+              element={
+                <PublicRoute>
+                  <h1>Public Route</h1>
+                </PublicRoute>
+              }
+            />
+            <Route path="marvel" element={<h1>Marvel Page</h1>} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    );
+    screen.debug();
+    expect(screen.queryByText("Marvel Page")).toBeTruthy();
+  });
+});
